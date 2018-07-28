208c6fb6ca71192c7a00fec84c1a6c0a10f75fb0
Leo Chu <leo_chu@hotmail.com>
2018-07-29 00:29:25 +0800

    #1 Tidy-up the code for easier maintenance

--- stick_driver.c
+++ stick_driver.c
@@ -1,16 +1,51 @@
+/*----------------------------------------------------------------------------|
+|                                                                             |
+|                               stick_driver.c                                |
+|                                                                             |
+|-----------------------------------------------------------------------------|
+|
+| Description  : Linux Kernel Driver Tutorial
+|
+|----------------------------------------------------------------------------*/
+
+
+
+/*----------------------------------------------------------------------------|
+|                           System Includes                                   |
+|-----------------------------------------------------------------------------|
+| Most of Linux drivers' header files are located at:                         | 
+| /usr/src/linux-headers-<your-linux-version>/include/                        |
+|----------------------------------------------------------------------------*/
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
-/*---------------------------------------------------------------------------*/
-/* MODULE PROTOTYPE */
+/*----------------------------------------------------------------------------|
+|                           Module Defines                                    |
+|----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------|
+|                           Module Type Definitions                           |
+|----------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------|
+|                           Module Function Prototypes                        |
+|----------------------------------------------------------------------------*/
+
+static int __init pen_init(void);
+
+static void __exit pen_exit(void);
 
 static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id);
 
 static void pen_disconnect(struct usb_interface *interface);
 
-/*---------------------------------------------------------------------------*/
-/* MODULE VARIABLE */
+/*----------------------------------------------------------------------------|
+|                           Module Global data                                |
+|----------------------------------------------------------------------------*/
 
 // usb_device_id
 static struct usb_device_id pen_table[] =
@@ -21,9 +56,11 @@ static struct usb_device_id pen_table[] =
 };
 MODULE_DEVICE_TABLE(usb, pen_table);
 
-
-//Leo: This is the most important LINKER of all related info for registering/deregistering USB device
-// usb_driver struct for holding USB driver information
+/* IMPORTANT usb_driver struct for registering 
+ * 1. Name of this USB driver
+ * 2. USB VID/PID this driver is intended for.
+ * 3. connect/disconnect callback handlers
+ */
 static struct usb_driver pen_driver =
 {
    .name = "LeoChu-USB Stick-Driver",
@@ -32,13 +69,29 @@ static struct usb_driver pen_driver =
    .disconnect = pen_disconnect     /* callback when disconnect USB */
 };
 
+/*----------------------------------------------------------------------------|
+|                           Module Function Definitions                       |
+|----------------------------------------------------------------------------*/
+//Leo(TODO): I am still not quite sure what these will go as they look like MACRO expansion to me.
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Leo Chu");
+MODULE_DESCRIPTION("USB Pen Registration Driver");
+
+module_init(pen_init);
+module_exit(pen_exit);
 
 
-
-/*---------------------------------------------------------------------------*/
-/* MODULE DEFINITION                                                         */
-
+/*#-------------------------------------------------------------------------#*/
 static int __init pen_init(void)
+/** @brief Initialization routine executed when this kernel module is loaded
+ *
+ *  This function must register to Linux kernel via "module_init(pen_init)" call
+ *
+ *  @param[in,out] input argument
+ *  @param[in,out] output argument
+ *  @return void.
+ */
 {
    int ret = -1;
 
@@ -50,16 +103,32 @@ static int __init pen_init(void)
    return ret;
 }
 
-
+/*#-------------------------------------------------------------------------#*/
 static void __exit pen_exit(void)
+/** @brief Termination routine executed when this kernel module is removed
+ *
+ *  This function must register to Linux kernel via "module_exit(pen_exit)" call
+ *
+ *  @param[in,out] input argument
+ *  @param[in,out] output argument
+ *  @return void.
+ */
 {
    printk(KERN_INFO "[*] LeoChu Destructor of driver");
    usb_deregister(&pen_driver);
    printk(KERN_INFO "\tDeregistration completed");
 }
 
-/* This function is only called when no other preceeding driver handle this device's insertion */
+/*#-------------------------------------------------------------------------#*/
 static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
+/** @brief One line description of the function
+ *
+ *  Detail description of the function
+ *
+ *  @param[in,out] input argument
+ *  @param[in,out] output argument
+ *  @return void.
+ */
 {
    printk(KERN_INFO "[*]  LeoChu Pen Drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
 
@@ -67,15 +136,18 @@ static int pen_probe(struct usb_interface *interface, const struct usb_device_id
    return 0;   /* return 0 indicates we will manage this device */
 }
 
+/*#-------------------------------------------------------------------------#*/
 static void pen_disconnect(struct usb_interface *interface)
+/** @brief One line description of the function
+ *
+ *  Detail description of the function
+ *
+ *  @param[in,out] input argument
+ *  @param[in,out] output argument
+ *  @return void.
+ */
 {
    printk(KERN_INFO "[*] LeoChu Pen Drive unplugged\n");
 }
 
-module_init(pen_init);
-module_exit(pen_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Leo Chu");
-MODULE_DESCRIPTION("USB Pen Registration Driver");
-
+/*#------------------------ End of stick_driver.c ---------------------------#*/
